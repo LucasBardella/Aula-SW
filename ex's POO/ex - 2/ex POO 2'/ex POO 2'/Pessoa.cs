44054d430476eff9886855b8c173150fcce238ac
Lucas Bardella <124801180+LucasBardella@users.noreply.github.com>
2024-04-11 09:02:57 -0300

    Add files via upload

---
+++ ex's POO/ex - 2/ex POO 2'/ex POO 2'/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_POO_2_
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public float altura { get; set; }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Data de Nascimento: " + dataNascimento);
+            Console.WriteLine("Altura: " + altura);
+        }
+
+        public int CalcularIdade()
+        {
+            return DateTime.Today.Year - dataNascimento.Year;
+        }
+        
+    }
+}
+ 
---
+++ ex's POO/ex - 2/ex POO 2'/ex POO 2'/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex_POO_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.nome = "Joel";
+            p.dataNascimento = new DateTime(1993, 6, 28);
+            p.altura = 1.76f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+            Console.ReadKey();
+        }
+    }
+}
